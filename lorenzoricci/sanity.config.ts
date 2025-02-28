c5e9266d0507ea4c42b7f0754432872d80ba112e
LorenzoRicci26 <riccilorenzo5@gmail.com>
2025-02-28 10:49:50 +0100

    set up next.js + sanity

--- lorenzoricci/sanity.config.ts
+++ lorenzoricci/sanity.config.ts
@@ -1,16 +1,17 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import {schemaTypes} from './schemas'
 
 export default defineConfig({
-  name: 'default',
+  name: 'lorenzoricci',
   title: 'Sanity Next.js Site',
 
   projectId: 'yd28xq2m',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  basePath: "/studio",
+
+  plugins: [structureTool()],
 
   schema: {
     types: schemaTypes,
