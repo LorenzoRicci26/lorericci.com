60d6c397e170e2645ddaffe9312d1796abeef6c1
LorenzoRicci26 <riccilorenzo5@gmail.com>
2025-03-01 21:45:11 +0100

    restructured app and studio

--- lorenzoricci/schemas/index.ts
+++ lorenzoricci/schemas/index.ts
@@ -2,5 +2,6 @@
 
 import profile from "./profile";
 import education from "./education";
+import project from "./project";
 
-export const schemaTypes = [profile, education];
+export const schemaTypes = [profile, education, project];
